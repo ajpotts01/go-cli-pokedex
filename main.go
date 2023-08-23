af3ee81afc1ad882a463a86e084216bb5898e66d
ajpotts01 <ajpotts01@gmail.com>
2023-08-23 22:15:55 +1000

    Convert commands to map

--- main.go
+++ main.go
@@ -2,15 +2,63 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
+
+type command struct {
+	name   string
+	desc   string
+	method func() error
+}
+
+func requestExit() error {
+	fmt.Println("Exiting")
+	os.Exit(0)
+	return nil
+}
+
+func requestHelp() error {
+	fmt.Println("Help")
+	return nil
+}
+
+func getCommands() map[string]command {
+	return map[string]command{
+		"help": {
+			name:   "Help",
+			desc:   "Help message",
+			method: requestHelp,
+		},
+		"exit": {
+			name:   "Exit",
+			desc:   "Exit Pokedex",
+			method: requestExit,
+		},
+	}
+}
+
+func printCommands() {
+	validCommands := getCommands()
+	for _, cmd := range validCommands {
+		fmt.Println(cmd.name)
+		fmt.Println(cmd.desc)
+	}
+}
+
+func handleRequest(request string) {
+	validCommands := getCommands()
+
+	command := validCommands[request]
+	command.method()
+}
 
 func main() {
 	for {
-		fmt.Print("pokedex >")
+		fmt.Print("pokedex > ")
 
 		var input string
 		cmd, err := fmt.Scanln(&input)
-		
+
 		cmdStr := fmt.Sprintf("Command: %v", cmd)
 		inputStr := fmt.Sprintf("Input: %s", input)
 
@@ -18,15 +66,7 @@ func main() {
 		fmt.Println(inputStr)
 
 		if err == nil {
-			switch input {
-				case "help":
-					fmt.Println("You selected Help")
-				case "exit":
-					fmt.Println("You selected Exit")
-					return
-				default:
-					fmt.Println("Command not recognised")
-			}
+			handleRequest(input)
 		} else {
 			print(err)
 		}
